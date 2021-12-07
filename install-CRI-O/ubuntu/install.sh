bab1f192b105a49d58dbe1fa897d0ab2077979a2
micki <github@com-science.de>
2021-12-07 20:06:23 +0100

    CRI-O: Enable installation of certain version

--- install-CRI-O/ubuntu/install.sh
+++ install-CRI-O/ubuntu/install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-
 # Installation instructions taken from https://kubernetes.io/docs/setup/production-environment/container-runtimes/
+
+if [ -z "$1" ]; then
+    echo "error: First argument (version) is missing"
+    exit 1;
+fi
+CRI_VERSION=$1
 
 # Create the .conf file to load the modules at bootup
 cat <<EOF | sudo tee /etc/modules-load.d/crio.conf
@@ -27,32 +32,53 @@ set -x
 . /etc/os-release
 
 OS=xUbuntu_${VERSION_ID}
-VERSION=1.22
 
 cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
 deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /
 EOF
-cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list
-deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /
+cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$CRI_VERSION.list
+deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$CRI_VERSION/$OS/ /
 EOF
 
-curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | \
+cd /tmp
+url=https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key
+if ! curl -OL ${url} ; then
+    echo "error: curl: could not download file from ${url}"
+    exit 1
+fi
+if grep -qs "Error 404" Release.key; then
+    echo "error: URL ${url} let to HTTP 404 error"
+    exit 1
+fi
+cat Release.key | \
 	sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
-curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/Release.key | \
+
+url=https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$CRI_VERSION/$OS/Release.key 
+if ! curl -OL ${url} ; then
+    echo "error: curl: could not download file from ${url}"
+    exit 1
+fi
+if grep -qs "Error 404" Release.key; then
+    echo "error: URL ${url} let to HTTP 404 error"
+    exit 1
+fi
+cat Release.key | \
 	sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers-cri-o.gpg add -
 
 sudo apt-get update
-sudo apt-get install -y cri-o cri-o-runc
+t=/tmp/cri-o.lst
+apt search cri-o 2>/dev/null | grep "^cri-o[-\.0-9]*\/" | cut -d'/' -f1 >$t
+if [[ $(cat $t | wc -l) > 1 ]]; then
+    cat $t | grep ${CRI_VERSION} >${t}~
+    mv ${t}~ $t
+    if [[ $(cat $t | wc -l) != 1 ]]; then
+        echo "error: multiple results for cri-o:"
+        cat $t
+        exit 1
+    fi
+fi
 
-# The [Container runtimes page](https://kubernetes.io/docs/setup/production-environment/container-runtimes) 
-# explains that the systemd driver is recommended for kubeadm based setups instead 
-# of the cgroupfs driver, because kubeadm manages the kubelet as a systemd service.
-# Therefore, do not enable cgroupfs usage here:
-# cat <<EOF | sudo tee /etc/crio/crio.conf.d/02-cgroup-manager.conf
-# [crio.runtime]
-# conmon_cgroup = "pod"
-# cgroup_manager = "cgroupfs"
-# EOF
+sudo apt-get install -y $(cat $t) cri-o-runc
 
 sudo systemctl daemon-reload
 sudo systemctl enable crio --now
